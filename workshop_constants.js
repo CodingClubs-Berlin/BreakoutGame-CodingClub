979d359870a178657a45a60c3a7568e847d5af0e
Johannes <johannes.timmrec@epitech.eu>
2020-10-02 15:55:27 +0200

    finished game

---
+++ workshop_constants.js
@@ -0,0 +1,13 @@
+
+const BLUE = "#31EFF2"
+
+//Arrow keys
+
+const ARROW_RIGHT = "ArrowRight"
+const ARROW_LEFT = "ArrowLeft"
+
+//Directions
+
+const NONE = "None"
+const RIGHT = "Right"
+const LEFT = "Left"
